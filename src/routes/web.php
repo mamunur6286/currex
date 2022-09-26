3ccb46edf2a6bf8c2ef45a96f369e1de08facb48
Md Mamunur Rashid <mamunur6286@gmail.com>
2022-09-26 13:32:05 +0600

    update

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('calculator', function(){
-    echo 'Hello from the calculator package!';
+    echo 'Hello from the calculator package! ';
 });
 
 Route::get('convert/{a}/{b}', 'Mamunur6286\Currex\Controllers\CorrencyConvertController@add');
